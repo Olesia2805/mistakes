e1a970a69d23ed1d9191564eb130fd212b79c48c
Olesia2805 <vysotina2002@gmail.com>
2024-04-07 12:57:25 +0200

    task

---
+++ general/task2.py
@@ -0,0 +1,16 @@
+"""
+def find_max():
+    numbers = input("Enter numbers separated by spaces: ")
+    max_number = max(numbers)
+    print("Maximum number:", max_number)
+
+find_max()
+"""
+
+def find_max():
+    numbers = input("Enter numbers separated by spaces: ")
+    data = [num for num in numbers.split(' ')]
+    max_number = max(data)
+    print("Maximum number:", max_number)
+
+find_max()
