1af514ed314b3b0df73a315ec5729758a538c548
Olesia2805 <vysotina2002@gmail.com>
2024-04-07 13:23:50 +0200

    task

---
+++ Python/general/power.py
@@ -0,0 +1,17 @@
+"""
+def calculate_power():
+    base = input("Enter a number: ")
+    exponent = input("Enter power: ")
+    result = base ** exponent
+    print("Result:", result)
+
+calculate_power()
+"""
+
+def calculate_power():
+    base = float(input("Enter a number: "))
+    exponent = float(input("Enter a power: "))
+    result = base ** exponent
+    print("Result:", result)
+
+calculate_power()
