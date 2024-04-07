a419531630af12b4f2e2cafcdbd52e87407f0c65
Olesia2805 <vysotina2002@gmail.com>
2024-04-07 12:40:49 +0200

    first commit

---
+++ general/task1.py
@@ -0,0 +1,17 @@
+"""
+def multiply_numbers():
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
+    result = num1 * num2
+    print("Multiplication result:", result)
+
+multiply_numbers()
+"""
+
+def multiply_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    result = num1 * num2
+    print("Multiplication result:", result)
+
+multiply_numbers()
