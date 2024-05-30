92e79ebff2f95b2e6113f5efb81d6226dd39d6b0
Olesia2805 <vysotina2002@gmail.com>
2024-05-30 22:22:54 +0200

    average func

---
+++ Python/general/average.py
@@ -0,0 +1,17 @@
+"""
+def calculate_average():
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+    average = num1 + num2 / 2
+    print("Середнє значення чисел:", average)
+
+calculate_average()
+"""
+
+def calculate_average():
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+    average = (num1 + num2) / 2
+    print("Середнє значення чисел:", average)
+
+calculate_average()
